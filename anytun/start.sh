077584c5a7e4b506c55f34793add97322480ebb3
coomsn <acstudycn@gmail.com>
2024-09-23 16:29:09 +0800

    update

--- anytun/start.sh
+++ anytun/start.sh
@@ -10,14 +10,10 @@ export PATH="/data/adb/magisk:/data/adb/ksu/bin:$PATH:/system/bin"
 
 source ${scripts_dir}/anytun.service
 
-if [ ! -f "${module_dir}/disable" ] && ip link | grep -Eq 'tun[0-9]'; then
-  log Info "The process is starting, please wait" > "${scripts_dir}/run.log"
-  echo "The process is starting, please wait"
-  stop_tun
+if [ ! -f "${module_dir}/disable" ]; then
   start_tun
 else
-  log Warn "Open the VPN and restart the module" > "${scripts_dir}/run.log"
-  echo "Open the VPN and restart the module"
+  echo "module not turned on"
 fi
 
 start_anytun.inotify() {
